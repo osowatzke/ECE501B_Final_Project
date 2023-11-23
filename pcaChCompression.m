cf3956e45e2ed1e511980b4828171fe683070f99
Owen <osowatzke@gmail.com>
2023-11-23 11:16:51 -0700

    Saving diagonal entries of S matrix only

--- pcaChCompression.m
+++ pcaChCompression.m
@@ -4,6 +4,6 @@ function [U,S,V,T] = pcaChCompression(imdata,r)
     imdata = imdata - T;
     [U,S,V] = svd(imdata);
     U = U(:,1:r);
-    S = S(1:r,1:r);
+    S = diag(S(1:r,1:r));
     V = V(:,1:r);
 end
--- pcaCompression.m
+++ pcaCompression.m
@@ -1,6 +1,6 @@
 function [U,S,V,T] = pcaCompression(imdata,r)
     U = zeros(size(imdata,1),r,size(imdata,3));
-    S = zeros(r,r,size(imdata,3));
+    S = zeros(r,1,size(imdata,3));
     V = zeros(size(imdata,2),r,size(imdata,3));
     T = zeros(1,1,size(imdata,3));
     for i = 1:size(imdata,3)
